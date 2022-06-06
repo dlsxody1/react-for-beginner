58e1c1668e93f9ac3603c3f7ffe03d89c861c99d
이름 <dlsxody1@naver.com>
2022-06-06 18:04:47 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  //event.target -> 이벤트가 발생되는 요소
+  //event.target.value -> 이벤트가 발생되는 요소안에 들어있는 값.
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    console.log(toDo, toDos );
+    //...currentArray -> 배열의 값들을 엘리멘트로 반환한다.
+    setToDo("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+       
+        <button>Add To Do</button>
+      </form>
+      <hr/>
+      <ul>
+      {toDos.map((toDo, index) => <li key={index}>{toDo}</li>)}
+      </ul>
+      
+
     </div>
   );
 }
